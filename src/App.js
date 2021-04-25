2da45014a13141cfdf6aa11e847cde925505107e
OscarSpila <oscarspila@hotmail.com>
2021-04-25 19:18:22 -0300

    deleteTask in memory
    Se agregó la funcionalidad de borrado de tarea. Todo en memoria por el momento

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState} from 'react' // IMR + TAB
-import { isEmpty } from 'lodash'
+import { isEmpty, size } from 'lodash'
 import shortid from 'shortid'
 
 function App() {
@@ -25,24 +25,45 @@ function App() {
 
   }
 
+  const deleteTask = (id) => {
+    const filteredTask = tasks.filter(task => task.id !== id)
+
+    setTasks(filteredTask)
+  }
+
   return (
   <div className="container mt-5">
     <h1>Tareas</h1>
     <hr/>
     <div className="row">
       <div className="col-8">
-        <h4 className="text-center">Lista de Tareas</h4>
-        <ul className="list-group">
-          {
-            tasks.map((task) => (
-               <li className="list-group-item" key={task.id}>
-                  <span className="lead">{task.name}</span>
-                  <button className="btn btn-danger btn-sm float-right mx-2">Elimnar</button>
-                  <button className="btn btn-warning btn-sm float-right">Editar</button>
-                </li>
-            ))           
-          }
-        </ul>
+        <h4 className="text-center">Lista de Tareas</h4>        
+        {
+          size(tasks) === 0 ? (
+            <h5 className="text-center">Aún no hay tareas programadas</h5>
+          ) : (
+            <ul className="list-group">
+              {
+                tasks.map((task) => (
+                  <li className="list-group-item" key={task.id}>
+                      <span className="lead">{task.name}</span>
+                      <button 
+                        className="btn btn-danger btn-sm float-right mx-2"
+                        onClick={() => deleteTask(task.id)}
+                        >
+                          Elimnar
+                      </button>
+                      <button 
+                        className="btn btn-warning btn-sm float-right"
+                        >
+                          Editar
+                      </button>
+                    </li>
+                ))           
+              }
+            </ul>
+          )
+        }  
       </div>
       <div className="col-4">
         <h4 className="text-center">Formulario</h4>
